55dfaf27aa33a83a14dc9136d0231d525893d7da
marwenjammazi171 <https://github.com/marwenjammazi.com>
2026-08-29 10:16:39 +0100

    chore: initialize MERN backend

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+const app=express()
+app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.get("/api/v1/health",(rea,res)=>{
+    res.status(200).json({
+        success : true ,
+        message:"Job tracker api is running "
+    })
+})
+export default app;
