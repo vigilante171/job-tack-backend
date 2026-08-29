45201c4ca577be30f2526329ea7c5596e406109b
marwenjammazi171 <https://github.com/marwenjammazi.com>
2026-08-29 16:04:05 +0100

    adding the validator and auth service

---
+++ src/services/auth.service.ts
@@ -0,0 +1,24 @@
+import Bcrypt from 'bcrypt'
+import User, { IUser } from '../models/user.js'
+import {RegisterInput} from '../validators/auth.validator.js'
+export const registerUser = async (input : RegisterInput) =>{
+    const {name , email ,password } = input ;
+    //making sure the email has no spaces 
+    const normalizedEmail = email.trim().toLowerCase()
+    const exsistingUser = await User.findOne({email:normalizedEmail})
+    if(exsistingUser){
+        throw new Error("Email already registered");
+    }
+    const hashedPassword =await Bcrypt.hash(password, 12);
+   const user = await User.create({
+    name:name.trim(),
+    email:normalizedEmail,
+    password :hashedPassword
+   }) 
+    return {
+    id: user._id,
+    name: user.name,
+    email: user.email,
+    role: user.role
+  };
+}
